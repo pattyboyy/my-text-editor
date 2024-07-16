d9068a8928cd40ba4b02342444d0e602ce06419c
pattyboyy <134738449+pattyboyy@users.noreply.github.com>
2024-07-16 12:09:41 -0600

    MVP

---
+++ client/src/js/index.js
@@ -0,0 +1,55 @@
+import { Workbox } from 'workbox-window';
+import Editor from './editor';
+import './database';
+import '../css/style.css';
+
+const main = document.querySelector('#main');
+main.innerHTML = '';
+
+const loadSpinner = () => {
+  const spinner = document.createElement('div');
+  spinner.classList.add('spinner');
+  spinner.innerHTML = `
+  <div class="loading-container">
+  <div class="loading-spinner" />
+  </div>
+  `;
+  main.appendChild(spinner);
+};
+
+const editor = new Editor();
+
+if (typeof editor === 'undefined') {
+  loadSpinner();
+}
+
+// Check if service workers are supported
+if ('serviceWorker' in navigator) {
+  // register workbox service worker
+  const workboxSW = new Workbox('/src-sw.js');
+  workboxSW.register();
+} else {
+  console.error('Service workers are not supported in this browser.');
+}
+
+// Handle butInstall button press
+const butInstall = document.getElementById('buttonInstall');
+
+window.addEventListener('beforeinstallprompt', (event) => {
+  window.deferredPrompt = event;
+  butInstall.classList.toggle('hidden', false);
+});
+
+butInstall.addEventListener('click', async () => {
+  const promptEvent = window.deferredPrompt;
+  if (!promptEvent) {
+    return;
+  }
+  promptEvent.prompt();
+  window.deferredPrompt = null;
+  butInstall.classList.toggle('hidden', true);
+});
+
+window.addEventListener('appinstalled', (event) => {
+  window.deferredPrompt = null;
+});
